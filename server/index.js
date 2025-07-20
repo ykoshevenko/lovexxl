9230613b1009380510f34a20b15945fb6217e073
ykoshevenko <ykoshevenko@list.ru>
2025-07-20 11:55:32 +0300

    это, конечно, капец

--- server/index.js
+++ server/index.js
@@ -3,14 +3,19 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Раздаём статические файлы из корня проекта
+// Middleware для статических файлов
 app.use(express.static(path.join(__dirname, '../')));
 
-// Все запросы перенаправляем на index.html (для SPA)
+// Базовый маршрут
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+// Все остальные запросы перенаправляем на index.html (для SPA)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
